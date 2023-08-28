fe4ede1ebc2d14d8608a08e989898e870486c16b
kristian <kristiantarnaj@gmail.com>
2023-08-28 11:27:25 +0200

    feat(bpmn): replaced tailwind with regular css for single-bpmn

--- packages/lib/src/single.wc.svelte
+++ packages/lib/src/single.wc.svelte
@@ -47,7 +47,7 @@
     }
 
     await service.createVersion(id, { xml });
-    
+
     dispatch('saved')
   }
 
@@ -77,126 +77,288 @@
   });
 </script>
 
-<div class="h-screen overflow-hidden">
-  {#if loading}
-    <div class="z-10 absolute w-full h-screen bg-white flex justify-center items-center gap-1">
-      <div class="animate-bounce">L</div>
-      <div class="animate-bounce">o</div>
-      <div class="animate-bounce">a</div>
-      <div class="animate-bounce">d</div>
-      <div class="animate-bounce">i</div>
-      <div class="animate-bounce">n</div>
-      <div class="animate-bounce">g</div>
-      <div class="animate-bounce">.</div>
-      <div class="animate-bounce">.</div>
-      <div class="animate-bounce">.</div>
-    </div>
-  {/if}
+{#if loading}
+  <div class="loading-overlay">
+    <div class="loading-spinner"></div>
+    Loading...
+  </div>
+{/if}
 
-  <div id="layout" class="flex flex-col w-full h-full">
-    <nav class="flex justify-between bg-white p-4 border-b-2">
-      <Button variant="outlined" on:click={() => dispatch('back')}>Back</Button>
-      <Button on:click={save} loading={saveLoading}>Save</Button>
-    </nav>
+<div id="layout" class="layout">
+  <nav class="navigation">
+    <a class="button button-outlined" href={baseLink}>Back</a>
+    <button class="button button-filled" on:click={save} class:loading={saveLoading}>
+      {#if !saveLoading}
+        Save
+      {:else}
+        <span class="loading-spinner"></span>
+        Loading...
+      {/if}
+    </button>
+  </nav>
 
-    <div class="flex-1 flex">
-      <div id="canvas" class="flex-1" />
+  <div class="canvas-container">
+    <div id="canvas" class="canvas" />
 
-      {#if instance && versionInstance}
-        <div class="relative w-64 border-l-2 {sidebar ? '' : 'w-0'}">
-          <button
-            class="absolute top-1/2 right-full -rotate-90 border-t-2 border-x-2 flex items-center translate-x-10 -translate-y-1/2 p-1 rounded-t-lg bg-white hover:bg-[#E4E4E4]"
-            on:click={() => (sidebar = !sidebar)}
-          >
-            <span class="px-4"> Details </span>
-            <span class="material-symbols-outlined rotate-90 border-b-2 p-1"> menu </span>
-          </button>
+    {#if instance && versionInstance}
+      <div class="sidebar" class:open={sidebar}>
+        <button class="sidebar-toggle"
+                on:click={() => (sidebar = !sidebar)}
+        >
+          <span class="sidebar-toggle-label"> Details </span>
+          <span class="sidebar-toggle-icon material-symbols-outlined"> menu </span>
+        </button>
 
-          <div class="flex items-center w-full h-14 bg-black/[2%] px-2 gap-2 border-b-2">
-            <span class="material-symbols-outlined text-3xl"> check_box_outline_blank </span>
+        <div class="sidebar-header">
+          <span class="material-symbols-outlined sidebar-header-icon"> check_box_outline_blank </span>
 
-            <div>
-              <h2 class="uppercase text-xs leading-4 font-bold truncate">BPMN</h2>
-              <p class="text-xs leading-4 truncate">Settings</p>
+          <div>
+            <h2 class="sidebar-header-title">DMN</h2>
+            <p class="sidebar-header-subtitle">Settings</p>
+          </div>
+        </div>
+
+        <details>
+          <summary>General</summary>
+          <div class="details-grid">
+            <div class="field-container">
+              <label for="name">Name</label>
+              <input id="name" bind:value={instance.name} required />
+            </div>
+
+            <div class="field-container">
+              <label for="description">Description</label>
+              <textarea id="description" rows="4" bind:value={instance.description} />
+            </div>
+          </div>
+        </details>
+
+        <details class="sidebar-details p-2 border-b-2 cursor-pointer">
+          <summary>Version</summary>
+          <div class="details-grid">
+            <div class="field-container">
+              <label for="version">Version Number</label>
+              <input type="number" id="version" bind:value={versionInstance.version} readonly />
             </div>
           </div>
 
-          <details class="p-2 border-b-2 cursor-pointer">
-            <summary class="text-sm">General</summary>
-            <div class="flex flex-col gap-2 mt-4">
-              <div class="flex flex-col gap-1">
-                <label for="name">Name</label>
-                <input id="name" bind:value={instance.name} required />
-              </div>
-
-              <div class="flex flex-col gap-1">
-                <label for="description">Description</label>
-                <textarea id="description" rows="4" bind:value={instance.description} />
-              </div>
+          <div class="details-grid">
+            <div class="field-container">
+              <label for="versionId">Version ID</label>
+              <input id="versionId" bind:value={versionInstance.version} readonly />
             </div>
-          </details>
-
-          <details class="p-2 border-b-2 cursor-pointer">
-            <summary class="text-sm">Trigger</summary>
-            <div class="flex flex-col gap-2 mt-4">
-              <div class="flex flex-col gap-1">
-                <label for="trigger">Trigger</label>
-                <select id="trigger" bind:value={instance.trigger}>
-                  <option value="">Select Trigger</option>
-                  {#each triggers as trigger}
-                    <option value={trigger.id}>{trigger.name}</option>
-                  {/each}
-                </select>
-              </div>
-
-              <div class="flex flex-col gap-1">
-                <label for="triggerCondition">Trigger Condition</label>
-                <textarea id="triggerCondition" rows="4" bind:value={instance.triggerCondition} />
-              </div>
-            </div>
-          </details>
-
-          <details class="p-2 border-b-2 cursor-pointer">
-            <summary class="text-sm">Version</summary>
-            <div class="flex flex-col gap-2 mt-4">
-              <div class="flex flex-col gap-1">
-                <label for="version">Version Number</label>
-                <input type="number" id="version" bind:value={versionInstance.version} readonly />
-              </div>
-            </div>
-          </details>
-        </div>
-      {/if}
-    </div>
+          </div>
+        </details>
+      </div>
+    {/if}
   </div>
 </div>
 
 <style>
-  .animate-bounce:nth-of-type(2) {
-    animation-delay: 0.04s;
+  .layout {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
   }
-  .animate-bounce:nth-of-type(3) {
-    animation-delay: 0.08s;
+
+  .navigation {
+    display: flex;
+    justify-content: space-between;
+    background-color: var(--background-primary);
+    padding: 1rem;
+    border-bottom: 2px solid var(--border-primary);
   }
-  .animate-bounce:nth-of-type(4) {
-    animation-delay: 0.12s;
+
+  .canvas-container {
+    display: flex;
   }
-  .animate-bounce:nth-of-type(5) {
-    animation-delay: 0.16s;
+
+  .canvas {
+    flex: 1 1 0;
   }
-  .animate-bounce:nth-of-type(6) {
-    animation-delay: 0.2s;
+
+  .sidebar {
+    position: relative;
+    border-left: 2px solid var(--border-primary);
+    white-space: nowrap;
   }
-  .animate-bounce:nth-of-type(7) {
-    animation-delay: 0.24s;
+
+  .sidebar:not(.open) {
+    width: 0;
   }
-  .animate-bounce:nth-of-type(8) {
-    animation-delay: 0.28s;
+
+  .sidebar.open {
+    width: 16rem;
   }
-  .animate-bounce:nth-of-type(9) {
-    animation-delay: 0.32s;
+
+  .sidebar-header {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 3.5rem;
+    background-color: rgba(0,0,0,.02);
+    padding: 0 .5rem;
+    gap: .5rem;
+    border-bottom: 2px solid var(--border-primary);
   }
-  .animate-bounce:nth-of-type(10) {
-    animation-delay: 0.36s;
+
+  .sidebar-header-icon {
+    font-size: 2rem;
+  }
+
+  .sidebar-header-title {
+    text-transform: uppercase;
+    font-size: .75rem;
+    line-height: 1rem;
+    font-weight: bold;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  .sidebar-header-subtitle {
+    font-size: .75rem;
+    line-height: 1rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  .sidebar-toggle {
+    position: absolute;
+    top: 50%;
+    right: 100%;
+    transform: translate(44px, -50%) rotate(-90deg);
+    width: 128px;
+    border-top: 2px solid var(--border-primary);
+    border-left: 2px solid var(--border-primary);
+    border-right: 2px solid var(--border-primary);
+    display: flex;
+    align-items: center;
+    padding: 0 .25rem;
+    border-top-left-radius: .25rem;
+    border-top-right-radius: .25rem;
+    background-color: var(--background-primary);
+  }
+
+  .sidebar-toggle:hover {
+    background-color: var(--background-secondary);
+  }
+
+  .sidebar-toggle-label {
+    padding: 0 1rem;
+  }
+
+  .sidebar-toggle-icon {
+    transform: rotate(90deg);
+    border-bottom: 2px solid var(--border-primary);
+    padding: .25rem;
+  }
+
+
+
+  details {
+    padding: .5rem;
+    border-bottom: 2px solid var(--border-primary);
+    cursor: pointer;
+  }
+
+  summary {
+    margin: -.5rem;
+    padding: .5rem;
+    font-size: .875rem;
+  }
+
+  summary:hover {
+    background-color: var(--background-secondary);
+  }
+
+  .details-grid {
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+    margin-top: 1rem;
+  }
+
+
+
+  .field-container {
+    display: flex;
+    flex-direction: column;
+    gap: .25rem;
+  }
+
+  label {
+    font-size: .75rem;
+    font-weight: bold;
+  }
+
+  input,
+  select,
+  textarea {
+    border: 2px solid var(--border-primary);
+    border-radius: .25rem;
+    padding: .25rem .5rem;
+    font-size: .875rem;
+  }
+
+  .button {
+    display: flex;
+    align-items: center;
+    height: 2.5rem;
+    border-radius: .25rem;
+    border: 2px solid transparent;
+    text-align: center;
+    min-width: 4rem;
+    padding: 0 1.5rem;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  .button-outlined {
+    border-color: var(--border-primary);
+  }
+
+  .button-filled {
+    background-color: var(--primary-color);
+    color: var(--text-on-primary);
+  }
+
+  .button.loading {
+    border-color: var(--primary-color);
+    background-color: transparent;
+    color: var(--text-primary);
+    gap: 1rem;
+    font-weight: 400;
+  }
+
+
+
+  .loading-overlay {
+    z-index: 100;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: var(--background);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .loading-spinner {
+    width: 1.5rem;
+    height: 1.5rem;
+    border: 4px solid var(--border-primary);
+    border-left-color: var(--primary-color);
+    border-radius: 50%;
+    animation: spin 1s linear infinite;
+  }
+
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
   }
 </style>
